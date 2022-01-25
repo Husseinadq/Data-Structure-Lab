c6dd070777bc45a4465dd18b6de6e4a2d581a983
Hussein Al-Mansouri <78476938+Husseinadq@users.noreply.github.com>
2022-01-25 17:00:15 +0300

    Lab 3 exercises

--- Tic-Tac-Toe/src/TicTacToe.java
+++ Tic-Tac-Toe/src/TicTacToe.java
@@ -17,14 +17,11 @@ public class TicTacToe {
             }
         }
     }
-    public void putMark(int i, int j) throws IllegalArgumentException {
-        if ((i < 0) || (i > 2) || (j < 0) || (j > 2))
-             throw new IllegalArgumentException("Invalid board position");
-        if (board[i][j] != Empty)
-             throw new IllegalArgumentException("Board position occupied");
+    public void putMark(int i, int j)  {
         board[i][j] = player;
         player =-player;
        }
+
     public boolean isWin(int mark) {
 
         return ((board[0][0] + board[0][1] + board[0][2] == mark*3)
@@ -61,14 +58,53 @@ public class TicTacToe {
              }
          return sb.toString( );
          }
-    public static void main(String[ ] args) {
+    public int input(){
         Scanner scanner=new Scanner(System.in);
+        int input=0;
+        System.out.println("Enter the number 1 to 9" );
+        input=scanner.nextInt();
+        return input;
+         }
+    public static void main(String[ ] args) {
+
          TicTacToe game = new TicTacToe( );
 
         for (int i = 0; i <9 ; i++) {
+            System.out.println("1|2|3\n-----\n4|5|6          Model\n-----\n7|8|9\n");
             System.out.println(game);
-            System.out.println("Enter the x,y");
-            game.putMark(scanner.nextInt(),scanner.nextInt());
+            switch (game.input()) {
+                case 1:
+                    game.putMark(0,0);
+                    break;
+                case 2:
+                    game.putMark(0,1);
+                    break;
+                case 3:
+                    game.putMark(0,2);
+                    break;
+                case 4:
+                    game.putMark(1,0);
+                    break;
+                case 5:
+                    game.putMark(1,1);
+                    break;
+                case 6:
+                    game.putMark(1,2);
+                    break;
+                case 7:
+                    game.putMark(2,0);
+                    break;
+                case 8:
+                    game.putMark(2,1);
+                    break;
+                case 9:
+                    game.putMark(2,2);
+                    break;
+                default:
+                    System.out.println("Input is out of ring");
+                    i--;
+                    break;
+            }
             if (i>3)
             {
                 int winningPlayer = game.winner( );
